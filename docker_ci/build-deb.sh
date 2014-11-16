7faeef8f9041796d45df926be97f18b6de8f58d7
Patrick Lucas <me@patricklucas.com>
2014-11-15 18:14:26 -0800

    Use rsync instead of git archive in build-deb.sh

--- docker_ci/build-deb.sh
+++ docker_ci/build-deb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir /root/monitormanager-copy
+rsync -a /root/monitormanager/ /root/monitormanager-copy/
+
 pushd /root/monitormanager-copy
-git -C /root/monitormanager archive HEAD | tar -x
 yes | mk-build-deps -ri
 dpkg-buildpackage -us -uc
 popd
